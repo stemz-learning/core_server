c9a29f09032b5c4576822524671e6d99c34d37a4
jingus-bingus <jholmes9@ucmerced.edu>
2025-06-25 20:47:40 -0700

    modified:   src/api/models/courseModel.js 	modified:   src/api/routes/worksheetRoutes.js

--- src/api/models/courseModel.js
+++ src/api/models/courseModel.js
@@ -3,11 +3,6 @@ const mongoose = require('mongoose');
 const courseSchema = new mongoose.Schema({
   name: { type: String, required: true },
   description: { type: String, required: true },
-  teacher_user_id: { type: mongoose.Schema.Types.ObjectId, required: true, ref: 'User' },
-  student_user_ids: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
-  course_ids: [{type: mongoose.Schema.Types.ObjectId, ref: 'Course'}], //New field
-  schedule: { type: String, required: false }, // New field
-  recommendedGradeLevel: { type: String, required: false }, // New field
   lesson_1: { type: Boolean, default: false },
   lesson_2: { type: Boolean, default: false },
   lesson_3: { type: Boolean, default: false },
--- src/api/routes/worksheetRoutes.js
+++ src/api/routes/worksheetRoutes.js
@@ -1,20 +1,23 @@
 const express = require('express');
-const { 
-  createWSProgress, 
-  getWSProgress,
-  updateWSProgress,
-} = require('../controllers/worksheetController');
+const worksheetController = require('../controllers/worksheetController');
 
 
 const router = express.Router();
 
 // Route to create a new worksheet progress
-router.post('/create', createWSProgress);
+router.post('/create', worksheetController.createWSProgress);
+
+router.get('/course/:courseId', worksheetController.getWorksheetsByCourseId);
+// Route to get all worksheets
+router.get('/', worksheetController.getAllWorksheets);
 
 // Route to get a single worksheet progress by email and worksheetId
-router.get('/get/email/:email/worksheetId/:worksheetId', getWSProgress); 
+router.get('/get/email/:email/worksheetId/:worksheetId', worksheetController.getWSProgress); 
 
 // Route to update a worksheet progress by email and worksheetId
-router.put('/update', updateWSProgress);
+router.put('/update', worksheetController.updateWSProgress);
+
+// Route to get all worksheets by classroomId
+router.get('/classroom/:classroomId', worksheetController.getWorksheetsByClassroomId);
 
 module.exports = router;
