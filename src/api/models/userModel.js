4e5f38a60ee18da261743ad9f44ba5bed46f22e9
sri-chandrasekaran <122851944+sri-chandrasekaran@users.noreply.github.com>
2025-07-01 09:49:45 -0700

    added role field

--- src/api/models/userModel.js
+++ src/api/models/userModel.js
@@ -5,6 +5,17 @@ const userSchema = new mongoose.Schema({
     name: { type: String, required: true },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
+    role: {
+        type: String, 
+        enum: ["student", "teacher"],
+        required: true
+    }, 
+    grade: {
+        type: String,
+        required: function () {
+            return this.role == 'student';
+        },
+    },
 });
 
 // Create the User model
